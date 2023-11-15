55324b6b3fc93b5a985a734efc01aa6448dbc912
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-15 11:42:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
+export default{
+  components:{
+    PageHeader,
+    PageMain,
+    PageFooter,
+  }
+}
+</script>
+
+<template>
+
+  <PageHeader/>
+ 
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+
+</style>
