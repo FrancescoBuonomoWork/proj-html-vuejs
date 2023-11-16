28d0814354b5169cf17d31fecd22c1bdc3943459
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-16 10:57:27 +0100

    add rating section

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -3,12 +3,14 @@ import HeroSection from './HeroSection.vue';
 import InfoSection from './InfoSection.vue';
 import AboutUsSection from './AboutUsSection.vue';
 import WatchlistSection from './WatchlistSection.vue';
+import RatingSection from './RatingSection.vue';
 export default{
     components: {
         HeroSection,
         InfoSection,
         AboutUsSection,
         WatchlistSection,
+        RatingSection,
     }
 }
 </script>
@@ -18,6 +20,7 @@ export default{
     <InfoSection/>
     <AboutUsSection/>
     <WatchlistSection/>
+    <RatingSection/>
     
 
  
---
+++ src/components/RatingSection.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <section class="rating-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div class="rating-wrapper">
+                        <span class="rating-number">158</span>
+                        <span class="rating-desc">Satisfield clients</span>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="rating-wrapper">
+                        <span class="rating-number">3+</span>
+                        <span class="rating-desc">Our Projects</span>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="rating-wrapper">
+                        <span class="rating-number">1,906</span>
+                        <span class="rating-desc">Cup of Coffee</span>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="rating-wrapper">
+                        <span class="rating-number">2</span>
+                        <span class="rating-desc">Our Awards</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+.rating-section {
+    color: $white;
+    background-image: url(../assets/Parallax-01.jpg);
+    background-position: 50% 80%;
+    
+    height: 250px;
+    .container,.row{
+        height: 100%;
+    }
+    .row{
+        align-items: center;
+    }
+    .rating-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-weight: 700;
+        .rating-number{
+            font-size: 48px;
+        }
+        .rating-desc{
+            margin-top: 5px;
+            text-transform: uppercase;
+            font-size: 12px;
+        }
+    }
+}
+</style>
