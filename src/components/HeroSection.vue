5193c66fb18b0cc62092a732f34d220dadfb3925
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-17 13:05:29 +0100

    add carosel in hero

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,59 @@
 <script>
 export default{
+    data(){
+        return{
+            currentIndex : 0,
+            slides:[
+                {
+                   imgSRC:'src/assets/bredcum-img.jpg',
+                   active: true
+                },
+                {
+                   imgSRC:'src/assets/Parallax-01.jpg',
+                   active: false
+                },
+                {
+                   imgSRC:'src/assets/Parallax-02.jpg',
+                   active: false
+                }
+            ]
+        }
+    },
+    methods:{
+        nextSlide(){
+            // let currentSlide = this.slides[0];
+            // let currentIndex = this.slides[0].id;
+            // console.log(currentSlide)
+            // currentSlide.active = false;
+            
+            // currentIndex=  currentIndex + 1;
+            // currentIndex.active = true;
+            console.log('next');
+            this.slides[this.currentIndex].active = false;
+
+            if(this.currentIndex === this.slides.length - 1){
+                
+                this.currentIndex = 0
+            } else{
+                this.currentIndex++
+            }
+            this.slides[this.currentIndex].active = true
+            // this.slides[1].active = false
+            // this.slides[2].active = true
+        },
+        prevSlide(){
+            console.log('prev');
+            this.slides[this.currentIndex].active = false;
+            if(this.currentIndex === 0){
+                
+                this.currentIndex = this.slides.length - 1
+            } else{
+                this.currentIndex--
+            }
+            this.slides[this.currentIndex].active = true
+        }
+    },
+   
     
 }
 </script>
@@ -8,13 +62,16 @@ export default{
     <section class="hero" >
         <div class="container-fuild ">
             <div class="hero-carousel-wrapper">
-                <img class="hero-img" src="../assets/bredcum-img.jpg">
+                <img v-for="(slide,i) in slides" :class="slide.active === true ? 'active': ''" class="hero-img" :src="slide.imgSRC">
+                <!-- <img class="hero-img " src="../assets/bredcum-img.jpg">
+                <img class="hero-img " src="../assets/bredcum-img.jpg"> -->
+                <!-- <img src="../assets/Parallax-01.jpg"> -->
                 
                 <span class="arrow-wrapper">
-                    <span class="arrow arrow-left" >
+                    <span @click="prevSlide()" class="arrow arrow-left" >
                         <img class="w10"  src="../assets/left-arrow.svg">
                     </span>
-                    <span class="arrow arrow-right" >
+                    <span class="arrow arrow-right" @click="nextSlide()" >
                         <img class="w10"  src="../assets/right-arrow.svg">
                     </span>
                 </span>
@@ -49,6 +106,11 @@ export default{
 .hero-img{
     height: 700px;
     object-fit: cover;
+    display: none;
+    width: 100%;
+    &.active{
+        display: block;
+    }
 }
 .hero-carousel-wrapper{
     position: relative;
